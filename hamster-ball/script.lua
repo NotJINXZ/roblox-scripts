e6e8140a6a6b386cd528ac5f7b46b8e5a998e657
JINXZ <58348317+NotJINXZ@users.noreply.github.com>
2022-09-03 09:04:20 -0400

    Update script.lua

--- hamster-ball/script.lua
+++ hamster-ball/script.lua
@@ -6,4 +6,4 @@ local JUMP_POWER = 60
 local JUMP_GAP = 0.3
 
 -- Script --
-loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NotJINXZ/roblox-scripts/main/hamster-ball/source.lua'))()
