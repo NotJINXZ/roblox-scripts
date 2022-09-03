5bd19738cb6b17a270329e88eb59e02c4ebf43a4
JINXZ <58348317+NotJINXZ@users.noreply.github.com>
2022-09-03 09:12:06 -0400

    Delete script.lua

--- hamster-ball/script.lua
+++
@@ -1,9 +0,0 @@
--- Hamster Ball Script v1 --
-
--- Settings --
-local SPEED_MULTIPLIER = 30
-local JUMP_POWER = 60
-local JUMP_GAP = 0.3
-
--- Script --
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NotJINXZ/roblox-scripts/main/hamster-ball/source.lua'))()
