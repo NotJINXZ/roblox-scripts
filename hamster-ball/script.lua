6c03e78725c58a818a2b66f1b29b1ebd891c49a7
JINXZ <58348317+NotJINXZ@users.noreply.github.com>
2022-09-03 09:03:13 -0400

    Create script.lua

---
+++ hamster-ball/script.lua
@@ -0,0 +1,9 @@
+-- Hamster Ball Script v1 --
+
+-- Settings --
+local SPEED_MULTIPLIER = 30
+local JUMP_POWER = 60
+local JUMP_GAP = 0.3
+
+-- Script --
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua'))()
